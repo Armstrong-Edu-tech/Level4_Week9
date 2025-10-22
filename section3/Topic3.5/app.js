b12d2232e686baaf606c979ab3dd6c11f6a0656c
Mohamed shaban <moha2023toedu@gmail.com>
2025-10-22 18:38:52 +0300

    add practical

--- section3/Topic3.5/app.js
+++ section3/Topic3.5/app.js
@@ -13,7 +13,7 @@ app.use(express.static("views")); // serve the Vue frontend
 app.use("/users", userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
-
-app.listen(3000, () => console.log("🚀 Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
